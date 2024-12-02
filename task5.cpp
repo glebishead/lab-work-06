f0a2ad42592f5ea28d1ebe69de05996992d39fc5
Gleb Kayumov <glebaskayumov@yandex.ru>
2024-12-02 14:45:49 +0300

    Update and rename task6.cpp to task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -21,15 +21,11 @@ bool is_all_digits_differ(int n) {
 
 int main()
 {
-    /*5. Дана последовательность натуральных чисел {Aj}j=1...n (n<=10000). 
-    Удалить из последовательности числа, 
-    все цифры которых различны, а среди оставшихся 
-    продублировать числа, произведение которых кратно 14.*/
     std::cout << "5." << std::endl;
     
     int matrix[NMAX] = {0};
     int n;
-    std::cout << "Введите количество элементов последовательности - n" << std::endl;
+    std::cout << "Enter number of elements - n" << std::endl;
     std::cin >> n;
     
     for (int i = 0; i < n; i++) {
@@ -39,11 +35,9 @@ int main()
     for (int i = 0; i < n; i++) {
         if (not is_all_digits_differ(matrix[i])) {
             matrix[i] = 0;
-            // заменим неподходящие на 0 
         }
     }
     
-    // перенесем 0 в конец
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             if (matrix[i] != 0 & matrix[j] != 0) break;
@@ -54,7 +48,6 @@ int main()
         }
     }
     
-    // удалим 0
     for (int i = 0; i < n; i++) {
         if (matrix[i] == 0) {
             n = i;
@@ -62,7 +55,6 @@ int main()
         }
     }
     
-    // добавим в конец пары чисел, произведение которых кратно 14
     int n_copy = n;
     for (int i = 0; i < n_copy; i++) {
         for (int j = i + 1; j < n_copy; j++) {
